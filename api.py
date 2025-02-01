182cf483f70657e9395dc716cd3434401aae5b40
manyasaini-iitm <23f3003686@ds.study.iitm.ac.in>
2025-02-01 23:16:54 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import json 
 
 app = FastAPI()
-app.add.middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins =["*"],
     allow_credentials = True,
